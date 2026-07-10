6c2cd97fd649aa04edb8dcf10252d6f796c40405
Victor francis <datruth14.vd@gmail.com>
2026-07-10 10:30:36 +0100

    remove all caching, fix card overflow on mobile

--- dev-server.php
+++ dev-server.php
@@ -1,4 +1,8 @@
 <?php
+header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Pragma: no-cache');
+header('Expires: 0');
+
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 // API routes
